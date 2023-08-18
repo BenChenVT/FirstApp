60aa5f981f2aaf4a5b4a9493706d185dba0db2fd
BenChenVT <xiangkechen1016@gmail.com>
2023-08-17 22:09:59 -0400

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Ben Chen on 8/17/23.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
